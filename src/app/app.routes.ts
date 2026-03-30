46c33d9f33a90ead868c9d7280bbee4ce30b8588
juanmafx <juanm_ramallo@hotmail.com>
2026-03-30 16:00:40 -0300

    Revert "Add Angular drug-categories feature module"
    This reverts commit 74dd50e7a9240382c1646929c420c5c7e4b3abe4.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'drug-categories'
-  },
-  {
-    path: 'drug-categories',
-    loadComponent: () =>
-      import('./features/drug-categories/drug-categories.component').then(
-        (m) => m.DrugCategoriesComponent
-      )
-  }
-];
+export const routes: Routes = [];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {}
+export class App {
+  protected readonly title = signal('angular-interview-prep');
+}
